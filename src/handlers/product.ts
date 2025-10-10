de8e42f9faa943e532425eed9a1fd2caed625ee5
Guadalupe <guadalupe.miranda997@gmail.com>
2025-10-10 19:27:48 -0300

    CRUD creado :)

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -7,6 +7,71 @@ export const createProduct = async (req : Request, res : Response) => {
         res.json({data: product})
     } catch(error){
         console.log(error)
-    }
-    
+    }  
 }
+
+export const getProducts = async (req: Request, res: Response) => {
+    try{
+        const products = await Product.findAll({
+            order: [['id', 'ASC']]
+        })
+        res.json({data: products})
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+export const getProductByID = async (req: Request, res: Response) => {
+    try{
+        const product = await Product.findByPk(req.params.id)
+
+        if(!product) {
+            return res.status(404).json({error: 'El producto no existe.'})
+        }
+
+        res.json({data: product})
+    } catch(error){
+        console.log(error)
+    }
+}
+
+export const editProductByID = async (req: Request, res: Response) => {
+    try{
+
+        const productByID = await Product.findByPk(req.params.id)
+
+        if(!productByID) {
+            return res.status(404).json({error: 'El producto no existe.'})
+        }
+
+        const product = await Product.update(
+            req.body, 
+            {
+                where: {id: req.params.id}
+            }
+        )
+
+        res.json({data: 'El producto se ha editado correctamente.'})
+
+    } catch(error){
+        console.log(error)
+    }
+}
+
+export const deleteProductByID = async (req: Request, res: Response) => {
+    try{
+        const productByID = await Product.findByPk(req.params.id)
+
+        if(!productByID) {
+            return res.status(404).json({error: 'El producto no existe.'})
+        }
+
+        await Product.destroy({
+            where: {id: req.params.id}
+        })
+
+        res.json({data: 'El producto se ha eliminado correctamente.'})
+    } catch(error){
+        console.log(error)
+    }
+}               
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
-import { body } from "express-validator";
-import { createProduct } from "./handlers/product";
+import { createProduct, deleteProductByID, editProductByID, getProductByID, getProducts } from "./handlers/product";
 import { handleInputErrors } from "./middleware";
+import { body, param } from "express-validator";
 import { Router } from "express";
 
 const router = Router();
@@ -13,6 +13,33 @@ router.post('/',
     body('price').custom(value => value >= 0).withMessage('El producto debe ser mayor o igual a cero.'),
     handleInputErrors, 
     createProduct
+)
+
+router.get('/',
+    getProducts
+);
+
+router.get('/:id',
+    param('id').isNumeric().withMessage('El id debe ser numerico.'),
+    handleInputErrors,
+    getProductByID
+);
+
+router.put('/:id',
+    param('id').isNumeric().withMessage('El id debe ser numerico.'),
+    body('name').notEmpty().withMessage('El nombre del producto no puede ir vacio.'),
+    body('price').isNumeric().withMessage('El precio debe ser numerico.'),
+    body('price').notEmpty().withMessage('El producto debe tener un precio.'),
+    body('price').custom(value => value >= 0).withMessage('El producto debe ser mayor o igual a cero.'),
+    body('abailability').isBoolean().withMessage('La disponibilidad debe ser ingresada.'),
+    handleInputErrors,
+    editProductByID
+);
+
+router.delete('/:id', 
+    param('id').isNumeric().withMessage('El id debe ser numerico.'),
+    handleInputErrors,
+    deleteProductByID
 )
 
 export default router
